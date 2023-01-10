4566d151bd493f79d7e09424c7937d4d608a2e30
Jordan <jordansampson23@gmail.com>
2023-01-10 11:02:48 -0600

    initialize variables with function

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ gameForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const formData = new FormData(gameForm)
     const data = Object.fromEntries(formData)
-    console.log(data)
+    initializeGame(data)
+    closeForm()
 })
+
+const initializeVariables = (data) => {
+    data.gameMode = +data.gameMode
+    data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    data.playerOne = 'X'
+    data.playerTwo = 'O'
+    data.currentPlayer = 'X'
+    data.gameOver = false
+}
+
+const initializeGame = (data) => {
+    initializeVariables(data)
+    console.log(data)
+}
+
+const updateTurnDisplay = () => {
+    const turnDisplay = document.querySelector('.displayTurn')
+    // Update current player turn
+}
 
 function openForm() {
     document.getElementById('formPopUp').style.display = 'block'

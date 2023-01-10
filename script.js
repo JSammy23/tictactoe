e9198f7ea2f0896b08fe31f0f23c7c528af9a386
Jordan <jordansampson23@gmail.com>
2023-01-10 10:36:38 -0600

    Open & close form functions added

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ gameForm.addEventListener('submit', (e) => {
     const data = Object.fromEntries(formData)
     console.log(data)
 })
+
+function openForm() {
+    document.getElementById('formPopUp').style.display = 'block'
+}
+
+function closeForm() {
+    document.getElementById('formPopUp').style.display = 'none'
+}
